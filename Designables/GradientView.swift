783e66a22526ad21a636b6680b3d828270492131
Tobias Conradi <github@tobias-conradi.de>
2014-11-15 22:28:20 +0100

    initial demo state

---
+++ Designables/GradientView.swift
@@ -0,0 +1,44 @@
+//
+//  GradientView.swift
+//  Designables
+//
+//  Created by Tobias Conradi on 15.11.14.
+//  Copyright (c) 2014 Tobias Conradi. All rights reserved.
+//
+
+import UIKit
+class GradientView: UIView {
+    
+    var startColor:UIColor = UIColor.lightGrayColor() {
+        didSet {
+            updateView()
+        }
+    }
+    var endColor:UIColor = UIColor.whiteColor() {
+        didSet {
+            updateView()
+        }
+    }
+    var horizontal:Bool = true {
+        didSet{
+            updateView()
+        }
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        updateView()
+    }
+    
+    func updateView() {
+        let gradientLayer = layer as CAGradientLayer
+        gradientLayer.colors = [startColor.CGColor,endColor.CGColor]
+        gradientLayer.startPoint = horizontal ? CGPointMake(0.0, 0.5) : CGPointMake(0.5, 0.0)
+        gradientLayer.endPoint = horizontal ? CGPointMake(1.0, 0.5) : CGPointMake(0.5, 1.0)
+    }
+    
+    override class func layerClass()->AnyClass {
+        return CAGradientLayer.self
+    }
+    
+}
---
+++ Designables/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Designables
+//
+//  Created by Tobias Conradi on 15.11.14.
+//  Copyright (c) 2014 Tobias Conradi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
